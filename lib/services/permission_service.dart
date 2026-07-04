acdabd489274c6fcbed5bcd218efcb3d86c9f3ff
alpha1207vj <gide749@gmail.com>
2026-07-04 04:58:31 +0100

    Completed Permission Service Construction

--- lib/services/permission_service.dart
+++ lib/services/permission_service.dart
@@ -1,16 +1,36 @@
 import 'package:permission_handler/permission_handler.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 
 class PermissionService 
 {
-   Future<PermissionStatus> getStatus (Permission permission)async 
+    Future<PermissionStatus> getStatus (Permission permission)async 
    {
      return await permission.status ; //Function used to check the status of the permission 
    }
 
-   Future <PermissionStatus> requestAccess(Permission permission) async
-   {
-    return await permission.request(); //Function to request the status in case of permission denied
-   }
+    static Future<PermissionStatus> requestStoragePermission() async {
+    
+    final androidInfo = await DeviceInfoPlugin().androidInfo;
+    final sdkVersion = androidInfo.version.sdkInt;
+
+    Permission storagePermission = sdkVersion >= 33 
+        ? Permission.audio 
+        : Permission.storage;
+
+    return await storagePermission.request();
+  }
+    static Future<PermissionStatus> requestNotificationPermission() async {
+    
+    final androidInfo = await DeviceInfoPlugin().androidInfo;
+    final sdkVersion = androidInfo.version.sdkInt;
+
+    // Notifications are automatically granted on Android 12 and below
+    if (sdkVersion < 33) {
+      return PermissionStatus.granted;
+    }
+
+    return await Permission.notification.request();
+  }
 
    Future<void> openSettings() async
    {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import audio_service
 import audio_session
 import cloud_firestore
 import connectivity_plus
+import device_info_plus
 import firebase_core
 import isar_community_flutter_libs
 import just_audio
@@ -20,6 +21,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
