513a35657fd04f2ba3d0647b9f81b59f7090b849
alpha1207vj <gide749@gmail.com>
2026-07-06 05:49:07 +0100

    feat(music_model): add factory constructor to transform native song instances

--- lib/models/music_modal.dart
+++ lib/models/music_modal.dart
@@ -1,30 +1,52 @@
 // ignore_for_file: must_be_immutable
 import "package:isar_community/isar.dart";
 import "package:equatable/equatable.dart";
+import "package:on_audio_query/on_audio_query.dart";
 
 part "music_modal.g.dart";
 
 @Collection(ignore: {'props'})
 class Song extends Equatable 
 {
-    Id id = Isar.autoIncrement;
+    Id? id = Isar.autoIncrement;
+    final String systemId;// Attribute tied to the Android indexation of the song on the device 
     final String title;
     final String? artist;
     final String filePath ;
-    final int? duration;
+    final Duration? duration;
     final DateTime dateAdded ;
-    final String? artworkPath;
-    final int? filesize;
+    final double? filesize;
 
     Song({
+        required this.systemId,
         required this.title,
         required this.filePath,
         this.artist,
         this.duration,
-        this.artworkPath,
         this.filesize,
-   
     }) : dateAdded = DateTime.now(); // Expression used in Dart to assign a specific value to an attribute of a class on the fly during the creation of a specific instance 
+
+
+    /// A factory constructor used to transform the native instance by running 
+    /// calculations and normalization changes on its properties.
+    factory Song.fromDeviceToNativeSongModel(SongModel nativeSongModel)
+    {
+      final rawBytes = nativeSongModel.size;
+      final double filesize = double.parse((rawBytes / (1024 * 1024)).toStringAsFixed(2));
+
+      return Song(
+          systemId: nativeSongModel.id.toString(),
+          title : nativeSongModel.title,
+          filePath: nativeSongModel.data,
+          artist: nativeSongModel.artist ?? "Unknown artist",
+          duration: Duration(
+           minutes : (nativeSongModel.duration ?? 0) ~/ 60000, //Complex Mathematical Process used to turn the raw files duration into minutes and seconds
+           seconds : ((nativeSongModel.duration ?? 0) ~/ 1000) % 60 
+          ),
+          filesize: filesize 
+      );
+    }
+
 
     @override
     List<Object?> get props => [id,title,dateAdded];// Equatable getter used here to specify how songs should be compared between one another
